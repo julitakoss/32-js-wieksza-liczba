ba28a58ce6c9763fadf36d252c0fbe3b9029a864
julita_koss@tlen.pl <julitakoss@github.com>
2020-02-03 11:11:23 +0100

    commit projektu

---
+++ main.js
@@ -0,0 +1,13 @@
+var num1 = 2;
+var num2 = 5;
+console.log(number(num1, num2));
+let num3, num4;
+function number(num3,num4)
+{
+    if (num3<num4) {
+        return num4;   
+    }
+    if(num3>num4){
+        return num3;
+    }
+}
